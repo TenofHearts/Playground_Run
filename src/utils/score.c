6654ffaa9992f16a82f18eee4abcbb253ec34ee0
ten_of_hearts <ye_jin2004@163.com>
2024-01-22 17:35:33 +0800

    fix speed

--- src/utils/score.c
+++ src/utils/score.c
@@ -2,13 +2,16 @@
 
 int stage = 0;
 
+SDL_Rect Score_rect = {0, 0, SCORE_BOARD_H, SCORE_BOARD_W};
+
 void Score_Update()
 {
     Score_Handle();
 #ifndef DISPLAY_TIME_
     const SDL_Color fg_w = {255, 255, 255, 255};
-    char score_stored[20] = {0};
-    sprintf(score_stored, "Score: %d", app.score);
+    char score_stored[10] = {0};
+    Print_Text(app.score_board, fg_w, "Score:", 50);
+    sprintf(score_stored, "%d", app.score);
     app.score_board.w = 20 * strlen(score_stored);
     Print_Text(app.score_board, fg_w, score_stored, 50);
 #endif
@@ -28,11 +31,25 @@ void Score_Handle()
     if (stage <= 1 && app.score > 250)
     {
         stage = 2;
-        app.speed = 10;
+        app.speed = 9;
     }
     if (stage <= 2 && app.score > 500)
     {
         stage = 3;
+        app.speed = 12;
+    }
+    if (stage <= 4 && app.score > 1000)
+    {
+        stage = 4;
         app.speed = 15;
+    }
+    if (stage <= 5 && app.score > 2000)
+    {
+        stage = 6;
+        app.speed++;
+    }
+    if (stage == 6 && app.speed < 20)
+    {
+        app.speed++;
     }
 }
