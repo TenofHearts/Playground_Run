01a5739e3cba33540d755163af46980df9854254
ten_of_hearts <ye_jin2004@163.com>
2024-02-08 16:37:25 +0800

    idk what

--- src/utils/init_game.c
+++ src/utils/init_game.c
@@ -24,7 +24,7 @@ void Init_Game()
 void Init_Window()
 {
     app.mute = 0;
-    app.baby_mode = 0;
+    app.baby_mode = 1;
     app.win_rect.x = 1750;
     app.win_rect.y = 0;
     app.win_rect.h = WIN_H;
